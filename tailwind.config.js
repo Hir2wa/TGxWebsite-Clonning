f66fb4b1e3a21b392283085bacd8cd2269e640bc
Alain <alainfabricehirwa@gmail.com>
2025-01-17 15:54:35 +0200

    ....

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: { mycustom: "70fr 30fr" },
+    },
   },
   plugins: [],
 };
